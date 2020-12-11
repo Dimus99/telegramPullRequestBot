f06459d885d83f330463461b9a303ec5350ca83b
Liza148 <r9Ln6azZ9gtXtFk>
2020-12-12 03:41:50 +0500

    ухх. добавил работу с гитхабом, скачивание пулл реквеста по ссылке на него

--- src/main/java/GitHubInteraction.kt
+++ src/main/java/GitHubInteraction.kt
@@ -1,6 +1,33 @@
+
+import java.io.File
+import java.io.IOException
+
+
 class GitHubInteraction {
-    fun GetProject(){
-        // сделать так, чтобы можно было по ссылке получить проект
+    fun downloadPullRequest(urlPullRequest : String){
+        //разделяем ссылку pull request на ссылку на проект и номер пулл реквеста
+        val urlSplit = urlPullRequest.split("/")
+        val urlProject = urlSplit.take(5).joinToString(separator = "/")
+        val pullNumber = urlSplit[6]
+        var directoryForGitProject = "./gitCopies"
+        val command1 = "git clone $urlProject ${urlSplit[3]}_${urlSplit[6]}"
+        val command2 = "git pull origin pull/$pullNumber/head"
+        //далее пишем команды в консоль
+        // возможно стоит исправить этот костыль с изменением directoryForGitProject
+        // этот костыль связан с неудобством нормально создать директорию
+        for (command in listOf(command1, command2)) {
+            try {
+                val proc = Runtime.getRuntime().exec(command,null, File(directoryForGitProject))
+                proc.waitFor()
+                proc.destroy()
+            } catch (e: IOException) {
+                e.printStackTrace()
+            } catch (e: InterruptedException) {
+                e.printStackTrace()
+            }
+            directoryForGitProject += "/${urlSplit[3]}_${urlSplit[6]}"
+        }
+        // сделать так, чтобы можно было по ссылке получить проект / готово
         // потом этот проект можно будет отправить на виртуальную машину
     }
 }
