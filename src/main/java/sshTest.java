e1485336fb80d052bd83ccc3bef65b7a9392836f
Liza148 <r9Ln6azZ9gtXtFk>
2020-12-18 15:32:53 +0500

    edit test

--- src/main/java/sshTest.java
+++ src/main/java/sshTest.java
@@ -5,12 +5,10 @@ import java.util.Scanner;
 
 public class sshTest {
     public static void main(String[] args) throws JSchException, IOException {
-        SshConnection ssh = new SshConnection("root", "password", "213.189.217.23", 22);
+        SshConnection ssh = new SshConnection("root", "pwd", "213.189.217.23", 22);
         Scanner input = new Scanner(System.in);
         String command;
-        System.out.println(ssh.executeCommand("/help"));
-        System.out.println(ssh.executeCommand("cd .."));
-        System.out.println(ssh.executeCommand("ls"));
+        System.out.println(ssh.executeCommand("cd .. && ls"));
         while (true)
         {
             command = input.nextLine();
