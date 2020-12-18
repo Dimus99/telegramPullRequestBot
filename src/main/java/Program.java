f69712e7f4ac001035621eab4ab3456ec36b8400
Liza148 <r9Ln6azZ9gtXtFk>
2020-12-18 13:37:35 +0500

    refactoring

--- src/main/java/Program.java
+++ src/main/java/Program.java
@@ -1,7 +1,4 @@
-import java.sql.SQLException;
-
-
-public class program {
+public class Program {
     public static void main(String[] args) {
         DataBase database = new DataBase("src/main/java/databases/users.db");
         TelegramBot bot = new TelegramBot(database);
