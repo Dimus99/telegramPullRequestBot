a96fbbf5ef555dfb2e6f28003a65f659bf607097
Liza148 <r9Ln6azZ9gtXtFk>
2020-12-18 15:28:10 +0500

    test test

--- src/main/java/sshTest.java
+++ src/main/java/sshTest.java
@@ -1,18 +1,20 @@
 import com.jcraft.jsch.JSchException;
-
 import java.io.IOException;
 import java.util.Scanner;
+
+
 public class sshTest {
     public static void main(String[] args) throws JSchException, IOException {
-        SshConnection ssh = new SshConnection("username", "password", "host", 88);
+        SshConnection ssh = new SshConnection("root", "password", "213.189.217.23", 22);
         Scanner input = new Scanner(System.in);
         String command;
+        System.out.println(ssh.executeCommand("/help"));
+        System.out.println(ssh.executeCommand("cd .."));
+        System.out.println(ssh.executeCommand("ls"));
         while (true)
         {
             command = input.nextLine();
             System.out.println(ssh.executeCommand(command));
         }
     }
-
-
 }
